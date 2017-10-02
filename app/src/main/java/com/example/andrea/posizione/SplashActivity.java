1011bd0f5b3aef34dacedabffc8c9f656473c516
riccardomaldini <riccardo.maldini@gmail.com>
2017-10-02 18:51:38 +0200

    aggiunta splashscreen

---
+++ app/src/main/java/com/example/andrea/posizione/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.example.andrea.posizione;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by Riccardo Maldini on 05/02/2017.
+ * Questa classe implementa una splash screen nel modo giusto, ovvero senza mostrare una view
+ * */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+    }
+
+}
